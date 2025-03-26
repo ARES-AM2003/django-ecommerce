dd748509a26d7f252c928312a22c045ecdb27e1e
ares <rochaksulu2002@gmail.com>
2025-03-26 16:56:31 +0545

    Initial commit

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ares-am/Desktop/project/E-Commerce-Website-in-Django/env
+set -gx VIRTUAL_ENV /home/ares-am/Desktop/project/django-ecommerce/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
